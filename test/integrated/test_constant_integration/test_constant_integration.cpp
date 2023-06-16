dfd65fd441bbd852d307196d7e75950f903aefdb
Michael Steck <steck@anabrid.com>
2023-06-16 15:41:15 +0200

    WIP test: simple constant integration test, but it's far from the expected behaviour

---
+++ test/integrated/test_constant_integration/test_constant_integration.cpp
@@ -0,0 +1,111 @@
+// Copyright (c) 2023 anabrid GmbH
+// Contact: https://www.anabrid.com/licensing/
+//
+// This file is part of the model-1 hybrid-controller firmware.
+//
+// ANABRID_BEGIN_LICENSE:GPL
+// Commercial License Usage
+// Licensees holding valid commercial anabrid licenses may use this file in
+// accordance with the commercial license agreement provided with the
+// Software or, alternatively, in accordance with the terms contained in
+// a written agreement between you and Anabrid GmbH. For licensing terms
+// and conditions see https://www.anabrid.com/licensing. For further
+// information use the contact form at https://www.anabrid.com/contact.
+//
+// GNU General Public License Usage
+// Alternatively, this file may be used under the terms of the GNU
+// General Public License version 3 as published by the Free Software
+// Foundation and appearing in the file LICENSE.GPL3 included in the
+// packaging of this file. Please review the following information to
+// ensure the GNU General Public License version 3 requirements
+// will be met: https://www.gnu.org/licenses/gpl-3.0.html.
+// For Germany, additional rules exist. Please consult /LICENSE.DE
+// for further agreements.
+// ANABRID_END_LICENSE
+
+#include <Arduino.h>
+#include <unity.h>
+
+#include "daq.h"
+#include "lucidac.h"
+#include "mode.h"
+
+using namespace blocks;
+using namespace daq;
+using namespace lucidac;
+using namespace mode;
+
+LUCIDAC luci{};
+OneshotDAQ daq_{};
+
+void setUp() {
+  // This is called before *each* test.
+}
+
+void tearDown() {
+  // This is called after *each* test.
+}
+
+void test_init() {
+  // Initialize mode controller (currently separate thing)
+  ManualControl::init();
+
+  // Put LUCIDAC start-up sequence into a test case, so we can assert it worked.
+  TEST_ASSERT(luci.init());
+  // Assert we have the necessary blocks
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.ublock, "U-Block not inserted");
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.cblock, "C-Block not inserted");
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.iblock, "I-Block not inserted");
+  // TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.m1block, "M1-Block not inserted");
+
+  // Calibrate
+  TEST_ASSERT(daq_.init(0));
+  delayMicroseconds(50);
+  TEST_ASSERT(luci.calibrate(&daq_));
+  delayMicroseconds(200);
+}
+
+void test_function() {
+  // Enable REF signals on U-Block
+  TEST_ASSERT(luci.ublock->use_alt_signals(blocks::UBlock::ALT_SIGNAL_REF_HALF));
+  // Route it once through to the integration module in M1 slot
+  luci.route(blocks::UBlock::ALT_SIGNAL_REF_HALF_INPUT, 0, +0.2f, MBlock::M1_INPUT(0));
+  // Connect result to an ADC to measure
+  uint8_t adc_channel = 1;
+  TEST_ASSERT(luci.ublock->connect(MBlock::M1_OUTPUT(0), adc_channel));
+
+  float ic_value = 0.0f;
+  auto* intblock = (MIntBlock*)(luci.m1block);
+  intblock->set_ic(0,ic_value);
+
+  // Write config to hardware
+  luci.write_to_hardware();
+  delayMicroseconds(100);
+
+  // Test IC result, except it is negated
+  ManualControl::to_ic();
+  delayMicroseconds(200);
+  ManualControl::to_halt();
+  auto data = daq_.sample();
+  TEST_ASSERT_FLOAT_WITHIN(0.1f, -ic_value, data[adc_channel]);
+
+  // Let it integrate
+  auto start = micros();
+  ManualControl::to_op();
+  /*
+  for (auto i = 0; i < 2; i++) {
+    daq_.sample();
+  }
+   */
+  while (micros() - start < 120) {}
+  ManualControl::to_halt();
+}
+
+void setup() {
+  UNITY_BEGIN();
+  RUN_TEST(test_init);
+  RUN_TEST(test_function);
+  UNITY_END();
+}
+
+void loop() {}
