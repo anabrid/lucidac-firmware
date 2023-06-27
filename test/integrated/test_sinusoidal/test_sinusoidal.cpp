d052c656e3f1b77d62b1c7244fa08046540a9656
Michael Steck <steck@anabrid.com>
2023-06-27 09:26:24 +0200

    test: sinusoidal circuit

---
+++ test/integrated/test_sinusoidal/test_sinusoidal.cpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2023 anabrid GmbH
+// Contact: https://www.anabrid.com/licensing/
+//
+// This file is part of the model-1 hybrid-controller firmware.
+//
+// ANABRID_BEGIN_LICENSE:GPL
+// Commercial License Usage
+// Licensees holding valid commercial anabrid licenses may use this file in
+// accordance with the commercial license agreement provided with the
+// Software or, alternatively, in accordance with the terms contained in
+// a written agreement between you and Anabrid GmbH. For licensing terms
+// and conditions see https://www.anabrid.com/licensing. For further
+// information use the contact form at https://www.anabrid.com/contact.
+//
+// GNU General Public License Usage
+// Alternatively, this file may be used under the terms of the GNU
+// General Public License version 3 as published by the Free Software
+// Foundation and appearing in the file LICENSE.GPL3 included in the
+// packaging of this file. Please review the following information to
+// ensure the GNU General Public License version 3 requirements
+// will be met: https://www.gnu.org/licenses/gpl-3.0.html.
+// For Germany, additional rules exist. Please consult /LICENSE.DE
+// for further agreements.
+// ANABRID_END_LICENSE
+
+#include <Arduino.h>
+#include <unity.h>
+
+#include "daq.h"
+#include "lucidac.h"
+#include "mode.h"
+
+using namespace blocks;
+using namespace daq;
+using namespace lucidac;
+using namespace mode;
+
+LUCIDAC luci{};
+OneshotDAQ daq_{};
+
+void setUp() {
+  // This is called before *each* test.
+}
+
+void tearDown() {
+  // This is called after *each* test.
+}
+
+void test_init() {
+  // Initialize mode controller (currently separate thing)
+  ManualControl::init();
+
+  // Put LUCIDAC start-up sequence into a test case, so we can assert it worked.
+  TEST_ASSERT(luci.init());
+  // Assert we have the necessary blocks
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.ublock, "U-Block not inserted");
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.cblock, "C-Block not inserted");
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.iblock, "I-Block not inserted");
+  // TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, luci.m1block, "M1-Block not inserted");
+
+  // Calibrate
+  TEST_ASSERT(daq_.init(0));
+  delayMicroseconds(50);
+  TEST_ASSERT(luci.calibrate(&daq_));
+  delayMicroseconds(200);
+}
+
+void test_function() {
+  auto *intblock = (MIntBlock *)(luci.m1block);
+
+  // Connect a sinusoidal solution
+  TEST_ASSERT(intblock->set_ic(0, 1));
+  TEST_ASSERT(intblock->set_ic(1, 0));
+  TEST_ASSERT(luci.route(MBlock::M1_OUTPUT(0), 0, -1.0f, MBlock::M1_INPUT(1)));
+  TEST_ASSERT(luci.route(MBlock::M1_OUTPUT(1), 1, +1.0f, MBlock::M1_INPUT(0)));
+  luci.write_to_hardware();
+  delayMicroseconds(100);
+
+  // Run it
+  mode::ManualControl::to_ic();
+  delayMicroseconds(120);
+  mode::ManualControl::to_op();
+  delayMicroseconds(6666);
+  mode::ManualControl::to_halt();
+}
+
+void setup() {
+  UNITY_BEGIN();
+  RUN_TEST(test_init);
+  RUN_TEST(test_function);
+  UNITY_END();
+}
+
+void loop() {}
