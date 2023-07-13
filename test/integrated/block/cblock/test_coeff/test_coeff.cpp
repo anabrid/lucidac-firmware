d1ee49def4dbfff351fd370d9844495f317318b2
Nick Baberuxki <baberuxki@anabrid.com>
2023-07-13 14:28:03 +0200

    Improve cBlock coeff test

--- test/integrated/block/cblock/test_coeff/test_coeff.cpp
+++ test/integrated/block/cblock/test_coeff/test_coeff.cpp
@@ -30,8 +30,6 @@
 #include "lucidac.h"
 #include "mode.h"
 
-#include "test_common.h"
-
 using namespace blocks;
 using namespace daq;
 using namespace lucidac;
@@ -39,6 +37,9 @@ using namespace mode;
 
 LUCIDAC luci{};
 OneshotDAQ daq_{};
+
+uint8_t coeff;
+float tolerance = 0.02;
 
 void setUp() {
   // This is called before *each* test.
@@ -70,53 +71,62 @@ void test_init() {
 void test_coeff() {
   auto *identityblock = (MIntBlock *)(luci.m1block);
 
-  for(uint8_t coeff=0; coeff<32; coeff++) {
+  float diff = 0;
+  float sum_diff = 0;
+  float max_diff = 0;
+  uint32_t num_diff = 0;
 
-    uint8_t adc_channel = coeff != 7 ? 7 : 0;
+  uint8_t adc_channel = coeff != 7 ? 7 : 0;
 
-    char buffer[128];
-    sprintf(buffer, "Coeff%d ADC%d", coeff, adc_channel);
-    TEST_MESSAGE(buffer);
+  // Reset
+  luci.reset(true);
+  delayMicroseconds(100);
 
-    // Reset
-    luci.reset(true);
-    delayMicroseconds(100);
+  // Enable REF signals on U-Block
+  TEST_ASSERT(luci.ublock->use_alt_signals(blocks::UBlock::ALT_SIGNAL_REF_HALF));
+  TEST_ASSERT(luci.ublock->connect(identityblock->M1_OUTPUT(0), UBlock::OUTPUT_IDX_RANGE_TO_ADC()[adc_channel]));
 
-    // Enable REF signals on U-Block
-    TEST_ASSERT(luci.ublock->use_alt_signals(blocks::UBlock::ALT_SIGNAL_REF_HALF));
-    luci.ublock->connect(identityblock->M1_OUTPUT(0), UBlock::OUTPUT_IDX_RANGE_TO_ADC()[adc_channel]);
+  // Route the result to an ACL output
+  TEST_ASSERT(luci.ublock->connect(identityblock->M1_OUTPUT(0), UBlock::IDX_RANGE_TO_ACL_OUT(5)));
 
-    if (ANABRID_TESTS_USE_ACL_OUT) {
-      // Route the result to an ACL output
-      luci.ublock->connect(identityblock->M1_OUTPUT(0), UBlock::IDX_RANGE_TO_ACL_OUT(5));
-    }
+  // Route identity block
+  TEST_ASSERT(luci.route(blocks::UBlock::ALT_SIGNAL_REF_HALF_INPUT, coeff, 0, identityblock->M1_INPUT(0)));
+  
+  luci.write_to_hardware();
+  delayMicroseconds(100);
 
-    // Route identity block
-    luci.route(blocks::UBlock::ALT_SIGNAL_REF_HALF_INPUT, coeff, 0, identityblock->M1_INPUT(0));
-    
-    luci.write_to_hardware();
-    delayMicroseconds(100);
+  for (float c = -1; c <= 1; c += 0.001) {
+    TEST_ASSERT(luci.cblock->set_factor(coeff,c));
+    luci.cblock->write_to_hardware();
+    delayMicroseconds(10);
 
-    for (float c = -1; c <= 1; c += 0.001) {
-      luci.cblock->set_factor(coeff,c);
-      luci.cblock->write_to_hardware();
-      delayMicroseconds(10);
+    float adc_value = daq_.sample()[adc_channel];
 
-      float adc_value = daq_.sample()[adc_channel];
-      TEST_ASSERT_FLOAT_WITHIN(ANABRID_TESTS_TARGET_PRECISION, c, -adc_value);
-
-      sprintf(buffer, "C = %f, ACD = %f", c, -adc_value);
-      TEST_MESSAGE(buffer);
-
-    }
+    diff = abs(c + adc_value);
+    sum_diff += diff;
+    max_diff = diff > max_diff ? diff : max_diff;
+    num_diff++;
   }
+  
+  char buffer[128];
 
+  sprintf(buffer, "Coeff %d | ACD %d | Mean Diff = %f | Max Diff  = %f", coeff, adc_channel, sum_diff/num_diff, max_diff);
+  TEST_MESSAGE(buffer);
+
+  sprintf(buffer, "Maximal Difference for Coeff %d was %f", coeff, max_diff);
+  TEST_ASSERT_MESSAGE(max_diff < tolerance, buffer);
 }
+
 
 void setup() {
   UNITY_BEGIN();
   RUN_TEST(test_init);
-  RUN_TEST(test_coeff);
+  char buffer[128];
+  sprintf(buffer, "Sweep through [-1, 1] for each Coefficient and check if actual value is within %f", tolerance);
+  TEST_MESSAGE(buffer);
+  for (coeff = 0; coeff < 32 ; coeff++) {
+    RUN_TEST(test_coeff);
+  }
   UNITY_END();
 }
 
