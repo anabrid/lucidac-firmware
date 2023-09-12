64822e753af00671b05457d36243239c498082a1
Michael Steck <steck@anabrid.com>
2023-09-12 10:42:27 +0200

    tests: qtmr gated changed to pin12 and QTMR1+2

--- test/qtmr/qtmr_gated/qtmr_gated.cpp
+++ test/qtmr/qtmr_gated/qtmr_gated.cpp
@@ -13,9 +13,9 @@ volatile uint32_t ticks, oflows, cap_index, cap_vals[CAP_MAX];
 
 void my_isr() { // capture and overflow
   Serial.println("interrupt");
-  if (TMR1_SCTRL2 & TMR_SCTRL_IEF) { // capture
+  if (TMR1_SCTRL1 & TMR_SCTRL_IEF) { // capture
     Serial.println("captured");
-    TMR1_SCTRL2 &= ~(TMR_SCTRL_IEF); // clear
+    TMR1_SCTRL1 &= ~(TMR_SCTRL_IEF); // clear
   }
   asm volatile("dsb"); // wait for clear  memory barrier
 }
@@ -24,10 +24,34 @@ void capture_init() {
   CCM_CCGR6 |= CCM_CCGR6_QTIMER1(CCM_CCGR_ON);
 
   /*
-   * Configure Timer 2 of first QTMR module to do input-gated counting
+   * Configure Timer 1 of first QTMR module to do input-gated counting
    */
 
-  TMR1_CTRL2 = 0; // stop
+  TMR1_CTRL1 = 0; // stop
+  TMR1_CNTR1 = 0; // reset counter
+  TMR1_SCTRL1 = 0;
+  TMR1_LOAD1 = 0;
+  TMR1_CSCTRL1 = 0;
+  TMR1_LOAD1 = 0;       // start val after compare
+  TMR1_COMP11 = 0xffff; // count up to this val, interrupt,  and start again
+  TMR1_CMPLD11 = 0xffff;
+
+  /*
+  TMR1_SCTRL1 = TMR_SCTRL_CAPTURE_MODE(1);  //rising
+  attachInterruptVector(IRQ_QTIMER1, my_isr);
+  TMR1_SCTRL1 |= TMR_SCTRL_IEFIE;  // enable interrupts
+  NVIC_ENABLE_IRQ(IRQ_QTIMER1);
+   */
+  TMR1_CTRL1 = TMR_CTRL_CM(0) | TMR_CTRL_PCS(8) | TMR_CTRL_SCS(1);
+  // Invert secondary signal (gating when HIGH, counting when LOW)
+  TMR1_SCTRL1 = TMR_SCTRL_IPS;
+
+  /*
+   * Configure Timer 2 of first QTMR module to cascade from timer 1
+   * This increases maximum counts from 16bit to 32bit
+   */
+
+  TMR1_CTRL2 = 0;
   TMR1_CNTR2 = 0; // reset counter
   TMR1_SCTRL2 = 0;
   TMR1_LOAD2 = 0;
@@ -36,41 +60,17 @@ void capture_init() {
   TMR1_COMP12 = 0xffff; // count up to this val, interrupt,  and start again
   TMR1_CMPLD12 = 0xffff;
 
-  /*
-  TMR1_SCTRL2 = TMR_SCTRL_CAPTURE_MODE(1);  //rising
-  attachInterruptVector(IRQ_QTIMER1, my_isr);
-  TMR1_SCTRL2 |= TMR_SCTRL_IEFIE;  // enable interrupts
-  NVIC_ENABLE_IRQ(IRQ_QTIMER1);
-   */
-  TMR1_CTRL2 = TMR_CTRL_CM(0) | TMR_CTRL_PCS(8) | TMR_CTRL_SCS(2);
-  // Invert secondary signal (gating when HIGH, counting when LOW)
-  TMR1_SCTRL2 = TMR_SCTRL_IPS;
-
-  /*
-   * Configure Timer 3 of first QTMR module to cascade from timer 2
-   * This increases maximum counts from 16bit to 32bit
-   */
-
-  TMR1_CTRL3 = 0;
-  TMR1_CNTR3 = 0; // reset counter
-  TMR1_SCTRL3 = 0;
-  TMR1_LOAD3 = 0;
-  TMR1_CSCTRL3 = 0;
-  TMR1_LOAD3 = 0;       // start val after compare
-  TMR1_COMP13 = 0xffff; // count up to this val, interrupt,  and start again
-  TMR1_CMPLD13 = 0xffff;
-
-  TMR1_CTRL3 = TMR_CTRL_CM(0) | TMR_CTRL_PCS(4 + 2) /* | TMR_CTRL_LENGTH*/;
+  TMR1_CTRL2 = TMR_CTRL_CM(0) | TMR_CTRL_PCS(4 + 1) /* | TMR_CTRL_LENGTH*/;
 
   /*
    * Enable timers
    */
 
-  // Put pin11 in QTimer mode
-  *(portConfigRegister(11)) = 1; // ALT 1
+  // Put pin12 in QTimer mode
+  *(portConfigRegister(12)) = 1; // ALT 1
   // Enable timers in reverse order
-  TMR1_CTRL3 |= TMR_CTRL_CM(7);
-  TMR1_CTRL2 |= TMR_CTRL_CM(3);
+  TMR1_CTRL2 |= TMR_CTRL_CM(7);
+  TMR1_CTRL1 |= TMR_CTRL_CM(3);
 }
 
 void setup() {
@@ -79,20 +79,20 @@ void setup() {
     ;
   delay(1000);
 
-  // Connect pin23 to pin11
+  // Connect pin23 to pin12
   pinMode(23, OUTPUT);
   digitalWriteFast(23, HIGH);
 
   capture_init();
 
-  PRREG(TMR1_SCTRL2);
-  PRREG(TMR1_CSCTRL2);
-  PRREG(TMR1_CTRL2);
-  PRREG(TMR1_LOAD2);
-  PRREG(TMR1_COMP12);
-  PRREG(TMR1_CMPLD12);
-  PRREG(TMR1_COMP22);
-  PRREG(TMR1_CMPLD22);
+  PRREG(TMR1_SCTRL1);
+  PRREG(TMR1_CSCTRL1);
+  PRREG(TMR1_CTRL1);
+  PRREG(TMR1_LOAD1);
+  PRREG(TMR1_COMP11);
+  PRREG(TMR1_CMPLD11);
+  PRREG(TMR1_COMP21);
+  PRREG(TMR1_CMPLD21);
 }
 
 void loop() {
@@ -109,16 +109,16 @@ void loop() {
   delayNanoseconds(delay_ns);
   digitalWriteFast(23, HIGH);
 
+  auto tmr1_cntr1 = TMR1_CNTR1;
+  Serial.println(tmr1_cntr1);
   auto tmr1_cntr2 = TMR1_CNTR2;
   Serial.println(tmr1_cntr2);
-  auto tmr1_cntr3 = TMR1_CNTR3;
-  Serial.println(tmr1_cntr3);
-  t = (tmr1_cntr3 * 0xFFFF + tmr1_cntr2) * 671 / 100;
+  t = (tmr1_cntr2 * 0xFFFF + tmr1_cntr1) * 671 / 100;
   Serial.println(t);
+  TMR1_CNTR1 = 0;
   TMR1_CNTR2 = 0;
-  TMR1_CNTR3 = 0;
+  PRREG(TMR1_CNTR1);
   PRREG(TMR1_CNTR2);
-  PRREG(TMR1_CNTR3);
 
   t_prev = t;
   delay(10000);
