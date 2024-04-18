ef45a5c939261ffe1c53a1ab5d1cd6770f45905c
Michael Steck <steck@anabrid.com>
2024-04-18 18:09:24 +0200

    misc(CBlock): add INPUT/OUTPUT_IDX_RANGE utility attributes like in UBlock

--- lib/platform-lucidac/src/block/cblock.h
+++ lib/platform-lucidac/src/block/cblock.h
@@ -35,6 +35,16 @@ public:
 
   static constexpr uint8_t NUM_COEFF = 32;
 
+  static constexpr std::array<uint8_t, NUM_COEFF> INPUT_IDX_RANGE() {
+    return {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+  };
+
+  static constexpr std::array<uint8_t, NUM_COEFF> OUTPUT_IDX_RANGE() {
+    return {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+  };
+
 protected:
   std::array<const functions::AD5452, NUM_COEFF> f_coeffs;
 
