c008b6aa15d2ca1f806b694c3e4a987e21e1cf79
SvenK <git+sveto@svenk.org>
2024-06-06 23:39:19 +0200

    Fix #102 "pio run -e native" just passes.
    Don't say that it does something useful but at least it doesn't crash.
    
    This commit does not yet have the native tests fixed. This requires something similar then pio env-dependent build_src_filter on the libraries.

---
+++ src/native_simulator.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 anabrid GmbH
+// Contact: https://www.anabrid.com/licensing/
+// SPDX-License-Identifier: MIT OR GPL-2.0-or-later
+
+#include <stdio.h>
+int main() { fprintf(stderr, "Main Firmware not yet supported in native simulator\n");  }
+
