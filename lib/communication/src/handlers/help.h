f6d3830948ddf1b2d4924342c0d56bf48d25650d
SvenK <git+sveto@svenk.org>
2024-06-24 02:55:22 +0200

    Protocol handlers: Small fixes

--- lib/communication/src/handlers/help.h
+++ lib/communication/src/handlers/help.h
@@ -9,7 +9,7 @@ namespace handlers {
 class HelpHandler : public MessageHandler {
 public:
   int handle(JsonObjectConst msg_in, JsonObject &msg_out) override {
-    msg_out["human_readable_info"] = "This is a JSON-Lines protocol described at https://anabrid.dev/docs/pyanabrid-redac/redac/protocol.html";
+    msg_out["human_readable_info"] = "This is a JSON-Lines protocol described at https://anabrid.dev/docs/hybrid-controller/";
 
     auto types_list = msg_out.createNestedArray("available_types");
     msg::handlers::Registry.write_handler_names_to(types_list);
--- lib/communication/src/handlers/ident.h
+++ lib/communication/src/handlers/ident.h
@@ -12,8 +12,8 @@ public:
   int handle(JsonObjectConst msg_in, JsonObject &msg_out) override {
       msg_out["serial"] = nvmconfig::VendorOTP().serial_number;
 
-      dist::write_to_json(msg_out["fw_build"]);
-      loader::flashimage::toJson(msg_out["fw_image"]);
+      dist::write_to_json(msg_out.createNestedObject("fw_build"));
+      loader::flashimage::toJson(msg_out.createNestedObject("fw_image"));
       return success;
   }
 };
