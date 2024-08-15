b5bcb6c5840fedc41a4a26c188fe98dd9eb3e19a
SvenK <git+sveto@svenk.org>
2024-08-15 15:42:09 +0200

    Fixing sinus test.
    Works now...

--- test/hardware/integrated/u-c-i-int/test_sinus/test.cpp
+++ test/hardware/integrated/u-c-i-int/test_sinus/test.cpp
@@ -9,6 +9,7 @@
 #define ANABRID_PEDANTIC
 
 #include "daq/daq.h"
+#include "io/io.h"
 #include "lucidac/lucidac.h"
 
 #include <iostream>
@@ -35,64 +36,78 @@ using namespace platform;
 
 LUCIDAC lucidac;
 
+void setUp() {
+  // This is called before *each* test.
+}
+
+void tearDown() {
+  // This is called after *each* test.
+}
+
+void test_init_and_blocks() {
+  bus::init();
+  io::init();
+  mode::ManualControl::init();
+
+  // In carrier_.init(), missing blocks are ignored
+  TEST_ASSERT(lucidac.init());
+  delayMicroseconds(50);
+
+  lucidac.reset(false);
+  // We do need certain blocks
+  for (auto &cluster : lucidac.clusters) {
+    TEST_ASSERT_NOT_NULL(cluster.ublock);
+    TEST_ASSERT_NOT_NULL(cluster.cblock);
+    TEST_ASSERT_NOT_NULL(cluster.iblock);
+    //TEST_ASSERT_NOT_NULL(cluster.shblock);
+  }
+  TEST_ASSERT_NOT_NULL(lucidac.ctrl_block);
+}
+
 void test_sinus() {
   auto cluster = lucidac.clusters[0];
 
-  // TODO: Detect where we have the M-Int block readily available at the correct position.
   // This assumes an M-Int-Block in slot M0
+  // TODO: Rewrite that it can use any M-Int-Block
+  auto mintblock = (blocks::MIntBlock*)cluster.m0block;
+  TEST_ASSERT(mintblock != nullptr);
+  TEST_ASSERT(mintblock->is_entity_type(blocks::MBlock::TYPES::M_INT8_BLOCK));
 
   // Determine which integrators to use
-  uint8_t i0 = 0;
-  uint8_t i1 = 1;
+  uint8_t i0 = 2;
+  uint8_t i1 = 3;
+
+  TEST_ASSERT(mintblock->set_ic_value(i0, 1));
+  TEST_ASSERT(mintblock->set_ic_value(i1, 0));
 
   // Which lanes to use
-  uint8_t l0 = 0;
-  uint8_t l1 = 1;
+  uint8_t l0 = 2;
+  uint8_t l1 = 3;
 
   TEST_ASSERT(cluster.route(i0, l0, +0.25, i1));
   TEST_ASSERT(cluster.route(i1, l1, -0.5,  i0));
 
-  // which ACL_OUT to use
-  uint8_t out0 = 0;
-  uint8_t out1 = 1;
+  uint8_t acl_lane = 24;
+  TEST_ASSERT(cluster.route(i0,   acl_lane, 1.0, 15));
+  TEST_ASSERT(cluster.route(i1, ++acl_lane, 1.0, 15));
 
-  const uint8_t acl_lane_start = 24;
-
-  TEST_ASSERT(lucidac.set_acl_select(out0, platform::LUCIDAC::ACL::EXTERNAL_));
-  TEST_ASSERT(lucidac.set_acl_select(out1, platform::LUCIDAC::ACL::EXTERNAL_));
-
-  // a cross-lane where to sink the ACL_IN to. We don't use it so
-  // just put it so some computing element on M1 and hope for the best.
-  // If you want to improve it, configure U/C block instead of using route()
-  // in the next linkes.
-  uint8_t sink = 15;
-
-  TEST_ASSERT(cluster.route(i0, acl_lane_start + out0, 1.0, sink));
-  TEST_ASSERT(cluster.route(i1, acl_lane_start + out1, 1.0, sink));
-
-  TEST_ASSERT_EQUAL(1, cluster.write_to_hardware());
+  TEST_ASSERT(cluster.write_to_hardware());
   delay(10);
 
-  for (;;) {
+  for(;;) {
+    TEST_MESSAGE("loop");
     mode::ManualControl::to_ic();
     delayMicroseconds(120);
     mode::ManualControl::to_op();
-    delayMicroseconds(5'000);
-    mode::ManualControl::to_halt();
+    delayMicroseconds(5000);
   }
-
 }
 
 
 void setup() {
   UNITY_BEGIN();
-
-  bus::init();
-  TEST_ASSERT(lucidac.init());
-
-  lucidac.reset(false);
+  RUN_TEST(test_init_and_blocks);
   RUN_TEST(test_sinus);
-
   UNITY_END();
 }
 
