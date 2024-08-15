2883c76014cd49734fa13264ca21e74e15769023
Michael Steck <steck@anabrid.com>
2024-08-15 18:08:28 +0200

    tests(mblock): parametrized tests for MMulBlock multiplication

--- test/hardware/integrated/u-c-i-sh-mul/test_mblock_mul_calibration/test.cpp
+++ test/hardware/integrated/u-c-i-sh-mul/test_mblock_mul_calibration/test.cpp
@@ -7,6 +7,7 @@
 #include <unity.h>
 
 #include "test_fmtlib.h"
+#include "test_parametrized.h"
 
 #include "daq/daq.h"
 #include "io/io.h"
@@ -57,12 +58,64 @@ void test_calibration() {
     }
 }
 
-void test_function() {
+void test_function(float x, float y) {
   // Calibration should not leave any connections dangling around
-  for (auto &cluster : carrier_.clusters) {
-    TEST_ASSERT(!cluster.ublock->is_anything_connected());
-    TEST_ASSERT(!cluster.iblock->is_anything_connected());
+  // That's why the first time we need to get ourselves some reference signals
+  static bool first_time = true;
+  if (first_time) {
+    first_time = false;
+    for (auto &cluster : carrier_.clusters) {
+      TEST_ASSERT(!cluster.ublock->is_anything_connected());
+      TEST_ASSERT(!cluster.iblock->is_anything_connected());
+    }
+    // Route a constant x and y to each multiplier in the system
+    for (auto &cluster : carrier_.clusters)
+      for (auto mblock : {cluster.m0block, cluster.m1block}) {
+        if (!mblock or !mblock->is_entity_type(MBlock::TYPES::M_MUL4_BLOCK))
+          continue;
+        for (auto slot_idx : MBlock::SLOT_INPUT_IDX_RANGE())
+          TEST_ASSERT(cluster.add_constant(blocks::UBlock::Transmission_Mode::POS_REF,
+                                           mblock->slot_to_global_io_index(slot_idx), 1.0f,
+                                           mblock->slot_to_global_io_index(slot_idx)));
+      }
+    // Write to hardware and calibrate
+    TEST_ASSERT(carrier_.write_to_hardware());
+    TEST_ASSERT(carrier_.calibrate_routes(&DAQ));
   }
+
+  // Set x and y by changing coefficients
+  for (auto &cluster : carrier_.clusters)
+    for (auto mblock : {cluster.m0block, cluster.m1block}) {
+      if (!mblock or !mblock->is_entity_type(MBlock::TYPES::M_MUL4_BLOCK))
+        continue;
+      for (auto slot_idx : MBlock::SLOT_INPUT_IDX_RANGE())
+        TEST_ASSERT(
+            cluster.cblock->set_factor(mblock->slot_to_global_io_index(slot_idx), slot_idx % 2 ? x : y));
+    }
+  // Write to hardware and calibrate (but calibrating offsets is enough)
+  TEST_ASSERT(carrier_.write_to_hardware());
+  TEST_ASSERT(carrier_.calibrate_offset());
+
+  // Route outputs to ADC and check for the expected value
+  bool error = false;
+  for (auto &cluster : carrier_.clusters)
+    for (auto mblock : {cluster.m0block, cluster.m1block}) {
+      if (!mblock or !mblock->is_entity_type(MBlock::TYPES::M_MUL4_BLOCK))
+        continue;
+      for (auto mul_idx : MMulBlock::MULTIPLIERS_OUTPUT_RANGE())
+        TEST_ASSERT(carrier_.set_adc_channel(mul_idx, mblock->slot_to_global_io_index(mul_idx)));
+      TEST_ASSERT(carrier_.write_to_hardware());
+
+      auto data = DAQ.sample();
+      for (auto mul_idx : MMulBlock::MULTIPLIERS_OUTPUT_RANGE()) {
+        if (fabs(data[mul_idx] - x * y) > 0.1 * x * y) {
+          error = true;
+          TEST_MESSAGE_FORMAT("ERROR cluster={} mblock-slot={} mul_idx={} output={} outside acceptable range!",
+                              cluster.cluster_idx, static_cast<uint8_t>(mblock->slot), mul_idx, data[mul_idx]);
+        }
+      }
+    }
+  TEST_ASSERT_FALSE(error);
 }
 
 void setup() {
@@ -73,7 +126,10 @@ void setup() {
   UNITY_BEGIN();
   RUN_TEST(test_init_and_blocks);
   RUN_TEST(test_calibration);
-  RUN_TEST(test_function);
+  RUN_PARAM_TEST(test_function, 1.0f, 1.0f);
+  RUN_PARAM_TEST(test_function, 0.75f, 0.75f);
+  RUN_PARAM_TEST(test_function, 0.5f, 0.5f);
+  RUN_PARAM_TEST(test_function, 0.1f, 0.1f);
   UNITY_END();
 }
 
