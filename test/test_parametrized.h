2d8b4cdfd0de5831792568aa8cf17795c5cd0717
Michael Steck <steck@anabrid.com>
2024-08-15 16:58:59 +0200

    feat(tests): RUN_PARAM_TEST for running parametrized tests

---
+++ test/test_parametrized.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 anabrid GmbH
+// Contact: https://www.anabrid.com/licensing/
+//
+// SPDX-License-Identifier: MIT OR GPL-2.0-or-later
+
+#pragma once
+
+// Use like RUN_PARAM_TEST(test_function, 5, "bla");
+#define RUN_PARAM_TEST(func, ...) \
+    RUN_TEST([]() { return func(__VA_ARGS__); })
