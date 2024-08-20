67e879c5f8147250a40357c222fe332f7bd64d28
Tobias Pflüger <tobiaspflueger@t-online.de>
2024-08-20 11:15:06 +0200

    tests: changed sh_measure to use averages

--- test/hardware/test_common.h
+++ test/hardware/test_common.h
@@ -15,16 +15,17 @@
 constexpr float ANABRID_TESTS_TARGET_PRECISION = 0.05f;
 constexpr bool ANABRID_TESTS_USE_ACL_OUT = true;
 
-std::array<float, 16> measure_sh_gain(platform::Cluster &cluster, daq::BaseDAQ *DAQ) {
+std::array<float, 16> measure_sh_gain(platform::Cluster &cluster, daq::BaseDAQ *DAQ,
+                                      unsigned int averages = 42, unsigned int averages_delay = 100) {
   std::array<float, 16> data{};
 
   // Measure all channels by using SH gain outputs and switching channels
   cluster.shblock->to_gain(blocks::SHBlock::GainChannels::ZERO_TO_SEVEN);
   delay(100);
-  auto data_zero_to_seven = DAQ->sample();
+  auto data_zero_to_seven = DAQ->sample_avg(averages, averages_delay);
   cluster.shblock->to_gain(blocks::SHBlock::GainChannels::EIGHT_TO_FIFTEEN);
   delay(100);
-  auto data_eight_to_fifteen = DAQ->sample();
+  auto data_eight_to_fifteen = DAQ->sample_avg(averages, averages_delay);
 
   std::copy(data_eight_to_fifteen.begin(), data_eight_to_fifteen.end(),
             std::copy(data_zero_to_seven.begin(), data_zero_to_seven.end(), data.begin()));
