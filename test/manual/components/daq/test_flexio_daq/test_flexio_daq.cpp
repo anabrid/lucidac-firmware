43971a3478bb315fa2b326a1f06c4e26b8eab924
Michael Steck <steck@anabrid.com>
2024-08-20 10:58:01 +0200

    tests: improve test_flexio_daq

--- test/manual/components/daq/test_flexio_daq/test_flexio_daq.cpp
+++ test/manual/components/daq/test_flexio_daq/test_flexio_daq.cpp
@@ -11,6 +11,8 @@
 #include <Arduino.h>
 #include <unity.h>
 
+#include "test_parametrized.h"
+
 // Include things with all private members made public,
 // to inspect internal variables without additional hassle.
 #define private public
@@ -37,17 +39,22 @@ public:
 
 DummyRunDataHandler dummy_run_data_handler{};
 
-void do_run(RunConfig run_config, DAQConfig daq_config) {
+void test_number_of_samples(RunConfig run_config, DAQConfig daq_config, //
+                            size_t absolute_number_of_samples, size_t last_number_of_samples) {
   TEST_ASSERT(daq_config.is_valid());
 
   std::string run_id{"550e8400-e29b-11d4-a716-446655440000"};
   Run run_{run_id, run_config, daq_config};
 
-  TEST_ASSERT(FlexIOControl::init(run_config.ic_time, run_config.op_time, mode::OnOverload::IGNORE,
-                                  mode::OnExtHalt::IGNORE));
+  // Reset DAQ before initializing FlexIOControl, which currently produces
+  // short low pulses on OP and triggers DAQ. That's not a problem, but it
+  // is confusing when looking at the signals with the oscilloscope.
   FlexIODAQ daq_{run_, daq_config, &dummy_run_data_handler};
   dummy_run_data_handler.num_of_data_vectors_streamed = 0;
   daq_.reset();
+
+  TEST_ASSERT(FlexIOControl::init(run_config.ic_time, run_config.op_time, mode::OnOverload::IGNORE,
+                                  mode::OnExtHalt::IGNORE));
   TEST_ASSERT(daq_.init(0));
   daq_.enable();
 
@@ -55,10 +62,8 @@ void do_run(RunConfig run_config, DAQConfig daq_config) {
   FlexIOControl::force_start();
 
   while (!FlexIOControl::is_done()) {
-    if (!daq_.stream()) {
-      TEST_FAIL_MESSAGE("daq streaming error");
-    }
-    if ((millis() - t_start) > 2000ull * (run_config.ic_time + run_config.op_time)) {
+    TEST_ASSERT_MESSAGE(daq_.stream(), "DAQ streaming error");
+    if ((millis() - t_start) > 2 * 1000ull * (run_config.ic_time + run_config.op_time)) {
       TEST_FAIL_MESSAGE("run is taking longer than it should");
     }
   }
@@ -76,13 +81,20 @@ void do_run(RunConfig run_config, DAQConfig daq_config) {
   mode::FlexIOControl::to_end();
 
   // DAQ finalize clears some pointers and stuff
-  daq_.finalize();
+  TEST_ASSERT(daq_.finalize());
+
+  // Check number of absolute and last samples seen
+  delayMicroseconds(5);
+  TEST_ASSERT_EQUAL(absolute_number_of_samples, dummy_run_data_handler.num_of_data_vectors_streamed);
+  TEST_ASSERT_EQUAL(last_number_of_samples, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
 }
 
 void one_time_setup() {}
 
 void setUp() {
   // This is called before *each* test.
+  // The tests assume a certain buffer size
+  TEST_ASSERT_EQUAL(dma::BUFFER_SIZE, 32 * 8);
 }
 
 void tearDown() {
@@ -90,56 +102,16 @@ void tearDown() {
   FlexIOControl::reset();
 }
 
-void test_get_number_of_data_vectors_in_buffer_few() {
-  do_run({1000, 42'000}, {1, 100'000});
-  TEST_ASSERT_EQUAL(5, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
-  TEST_ASSERT_EQUAL(5, dummy_run_data_handler.num_of_data_vectors_streamed);
-}
-
-void test_get_number_of_data_vectors_in_buffer_none() {
-  // When buffer was filled exactly once
-  TEST_ASSERT_EQUAL(dma::BUFFER_SIZE, 32 * 8);
-  do_run({1000, 32 * 10'000}, {8, 100'000});
-  TEST_ASSERT_EQUAL(0, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
-  TEST_ASSERT_EQUAL(32, dummy_run_data_handler.num_of_data_vectors_streamed);
-}
-
-void test_get_number_of_data_vectors_in_buffer_one() {
-  do_run({1000, 3500}, {1, 100'000});
-  TEST_ASSERT_EQUAL(1, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
-  TEST_ASSERT_EQUAL(1, dummy_run_data_handler.num_of_data_vectors_streamed);
-}
-
-void test_get_number_of_data_vectors_in_buffer_one_different() {
-  do_run({1000, 42000}, {1, 10'000});
-  TEST_ASSERT_EQUAL(1, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
-  TEST_ASSERT_EQUAL(1, dummy_run_data_handler.num_of_data_vectors_streamed);
-}
-
-void test_get_number_of_data_vectors_in_buffer_two_very_close_timing() {
-  // When last sample is taken basically exactly when OP ends
-  do_run({1000, 10'000}, {1, 100'000});
-  TEST_ASSERT_EQUAL(2, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
-  TEST_ASSERT_EQUAL(2, dummy_run_data_handler.num_of_data_vectors_streamed);
-}
-
-void test_get_number_of_data_vectors_in_buffer_few_once_overflowed() {
-  TEST_ASSERT_EQUAL(dma::BUFFER_SIZE, 32 * 8);
-  do_run({1000, 33 * 10'000}, {8, 100'000});
-  TEST_ASSERT_EQUAL(1, ContinuousDAQ::get_number_of_data_vectors_in_buffer());
-  TEST_ASSERT_EQUAL(33, dummy_run_data_handler.num_of_data_vectors_streamed);
-}
-
 void setup() {
   one_time_setup();
   delayMicroseconds(100);
   UNITY_BEGIN();
-  RUN_TEST(test_get_number_of_data_vectors_in_buffer_few);
-  RUN_TEST(test_get_number_of_data_vectors_in_buffer_none);
-  RUN_TEST(test_get_number_of_data_vectors_in_buffer_one);
-  RUN_TEST(test_get_number_of_data_vectors_in_buffer_one_different);
-  RUN_TEST(test_get_number_of_data_vectors_in_buffer_two_very_close_timing);
-  RUN_TEST(test_get_number_of_data_vectors_in_buffer_few_once_overflowed);
+  RUN_PARAM_TEST(test_number_of_samples, {1000, 42'000}, {1, 100'000}, 5, 5);
+  RUN_PARAM_TEST(test_number_of_samples, {1000, 32 * 10'000}, {8, 100'000}, 32, 0);
+  RUN_PARAM_TEST(test_number_of_samples, {1000, 3500}, {1, 100'000}, 1, 1);
+  RUN_PARAM_TEST(test_number_of_samples, {1000, 42000}, {1, 10'000}, 1, 1);
+  RUN_PARAM_TEST(test_number_of_samples, {1000, 10'000}, {1, 100'000}, 2, 2);
+  RUN_PARAM_TEST(test_number_of_samples, {1000, 33 * 10'000}, {8, 100'000}, 33, 1);
   UNITY_END();
 }
 
