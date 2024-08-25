a07a92788e0a026386e4031ded1f621ebe2cd4de
SvenK <git+sveto@svenk.org>
2024-08-26 01:16:33 +0200

    Slight improvement in PrintMultiplexer.
    As written many times before, this is an awful class design. But it is just too handy for the time being.

--- lib/controller/src/utils/print-multiplexer.h
+++ lib/controller/src/utils/print-multiplexer.h
@@ -11,22 +11,24 @@
 #include <algorithm>
 #include <list>
 #include <vector>
+#include <QNEthernetClient.h>
 
 #include "util/PrintUtils.h" // QNEthernet
 
-//#include "utils/logging.h"
-
 namespace utils {
+
+using qindesign::network::EthernetClient;
 
 /**
  * A "multiplexer" for Print targets.
  *
  * This class tries to do something which does not properly work this way,
- * as it would require buffering. This code naively keeps "fingers crossed",
- * at least does some basic error reporting.
- *
- * TODO: Could be improved with accepting EthernetClients which have a writeFully
- *       method.
+ * as it would require buffering. This code naively keeps "fingers crossed"
+ * because even our error reporting (logging) eventually calls it, so there is
+ * no suitable error reporting anyway.
+ * 
+ * Methods in this class lie about their return value. This will soak up
+ * data silently. Or result in multiple prints, depending on your preference.
  *
  * TODO: This code is awful. Improve in any manner.
  *
@@ -34,63 +36,42 @@ namespace utils {
  * more sane.
  */
 class PrintMultiplexer : public Print {
-  std::list<Print *> targets;
-  /*
-  std::vector<size_t> ret_values;
-
-  void prepare_ret_values(size_t expected) {
-    if(ret_values.size() != targets.size()) {
-      ret_values.resize(targets.size());
-    }
-    for(size_t i=0; i < ret_values.size(); i++)
-      ret_values[i] = expected;
-  }
-  */
+  std::list<Print *> print_targets;
+  std::list<EthernetClient*> eth_targets;
 
 public:
-  void add(Print *target) { targets.push_back(target); }
+  bool optimistic = false; ///< whether you prefer to send too much or too few
 
-  void remove(Print *target) { targets.remove(target); }
+  void add(Print *target) { print_targets.push_back(target); }
+  void add(EthernetClient *target) { eth_targets.push_back(target); }
 
-  size_t size() const { return targets.size(); }
+  void remove(Print *target) { print_targets.remove(target); }
+  void remove(EthernetClient *target) { eth_targets.push_back(target); }
 
-  // Printables which go to all clients
+  size_t size() const { return print_targets.size() + eth_targets.size(); }
+
+  /// Printables which go to all clients
   virtual size_t write(uint8_t b) override {
-    // prepare_ret_values(1);
     bool success = true;
-    for (auto &target : targets)
-      if (target)
-        success &= target->write(b) == 1;
-    // if(!std::all_of(ret_values.cbegin(), ret_values.cend(), [](size_t i) { return i == 1; })) {
-    //  TODO: For circular dependency reasons, cannot use the Logging facilities because
-    //        this is part of it.
-    // LOG_ALWAYS("PrintMultiplexer: write(byte) failed for at least one target.");
-    //}
-    return success ? 1 : 0; // ret_values.size()>0 ? ret_values[0] : /*devnull*/ 1;
+    for (auto &target : print_targets) if (target) success &= target->write(b) == 1;
+    for (auto &target :   eth_targets) if (target) success &= target->write(b) == 1;
+    return success ? 1 : (optimistic ? 1 : 0);
   }
 
   size_t write(const uint8_t *buffer, size_t size) override {
-    // prepare_ret_values(size);
     bool success = true;
-    for (auto &target : targets)
-      if (target)
-        success &= qindesign::network::util::writeFully(*target, buffer, size) == size;
-    // if(!std::all_of(ret_values.cbegin(), ret_values.cend(), [size](size_t i) { return i == size; })) {
-    //  TODO: For circular dependency reasons, cannot use the Logging facilities because
-    //        this is part of it.
-    // LOG_ALWAYS("PrintMultiplexer: write(buffer,size) failed for at least one target.");
-
-    // Serial.printf("size=%d\n", size);
-    // for(auto & r : ret) { Serial.printf("ret = %d\n", r); }
-    //      }
-    return success ? size : 0;
-    // return ret_values.size()>0 ? ret_values[0] : /*devnull*/ size;
+    for (auto &target : print_targets) if (target) success &= qindesign::network::util::writeFully(*target, buffer, size) == size;
+    for (auto &target :   eth_targets) if (target) success &= target->writeFully(buffer, size) == size;
+    // so do all this extra work of print_targets + eth_targets just to exploit that
+    // EthernetClient::write_fully internally does a "still connected" check and if
+    // not, fails. This way we avoid an endless loop if a client disconnects during printout.
+    // Typically, in contrast, print_targets are either Serial or String-Buffers.
+    return success ? size : (optimistic ? size : 0);
   }
 
   virtual void flush() override {
-    for (auto &target : targets)
-      if (target)
-        target->flush();
+    for (auto &target : print_targets) if (target) target->flush();
+    for (auto &target :   eth_targets) if (target) target->flush();
   }
 };
 
