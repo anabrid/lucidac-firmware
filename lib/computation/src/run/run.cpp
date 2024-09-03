599fe3a2de653251138630d70646b6c43f143895
SvenK <git+sveto@svenk.org>
2024-09-03 10:59:48 +0200

    Really allow for large OP/IC times.
    ArduinoJSON is here the bottleneck, cannot work well with 64bit numbers.

--- lib/computation/src/run/run.cpp
+++ lib/computation/src/run/run.cpp
@@ -7,9 +7,27 @@
 #include <utility>
 
 run::RunConfig run::RunConfig::from_json(JsonObjectConst &json) {
+  // ATTENTION: ArduinoJSON cannot easily handle 64bit integers,
+  //        cf. https://arduinojson.org/v6/api/config/use_long_long/
+  // Therefore, client libraries should make use of appropriate units
+  // which are summed here together.
+
+  uint32_t
+    ic_time_ms  = json["ic_time_ms"],
+    ic_time_us  = json["ic_time_us"],
+    ic_time_ns  = json["ic_time_ns"],
+    ic_time_def = json["ic_time"], // Nanoseconds per default
+
+    op_time_ms  = json["op_time_ms"],
+    op_time_us  = json["op_time_us"],
+    op_time_ns  = json["op_time_ns"],
+    op_time_def = json["op_time"]; // Nanoseconds per default
+
+  uint64_t us = 1000, ms = 1'000'000;
+
   return {
-      .ic_time = json["ic_time"],
-      .op_time = json["op_time"],
+      .ic_time = ic_time_def + ic_time_ns + us*ic_time_us + ms*ic_time_ms,
+      .op_time = op_time_def + op_time_ns + us*op_time_us + ms*op_time_ms,
       .halt_on_overload = json["halt_on_overload"],
       .no_streaming = json["no_streaming"],
       .repetitive = json["repetitive"]
