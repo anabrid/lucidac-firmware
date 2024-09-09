9380f5ce1deb19135736ed45ffc778bcc9c13277
Tobias Pflüger <tobiaspflueger@t-online.de>
2024-09-09 17:54:11 +0200

    fix(status): Reduced RAM consumption by offloading strings to flash memory.

---
+++ lib/controller/src/utils/error.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 anabrid GmbH
+// Contact: https://www.anabrid.com/licensing/
+// SPDX-License-Identifier: MIT OR GPL-2.0-or-later
+
+#include "error.h"
+
+char utils::buf[utils::bufsize];
--- lib/controller/src/utils/error.h
+++ lib/controller/src/utils/error.h
@@ -4,52 +4,51 @@
 
 #pragma once
 
-#include <string>
+#include <Arduino.h>
 #include <cstdarg>
+#include <string>
 
 namespace utils {
+constexpr unsigned int bufsize = 1000;
+extern char buf[bufsize];
 
-    template<typename... Args>
-    std::string small_sprintf(const char* format, Args... args) {
-        constexpr unsigned int bufsize=1000;
-        char buf[bufsize];
-        snprintf(buf, bufsize, format, args...);
-        return buf;
-    }
+template <typename... Args> std::string small_sprintf(const char *format, Args... args) {
+  snprintf(buf, bufsize, format, args...);
+  return buf;
+}
 
-    /**
-     * A recoverable error, inspired from https://abseil.io/docs/cpp/guides/status
-     * and https://github.com/abseil/abseil-cpp/blob/master/absl/status/status.h
-     * 
-     * This class should be used as immutable.
-     * 
-     * For making the success path cheap, use status::success.
-     **/
-    class status {
-    public:
-        int code;
-        std::string msg;
+/**
+ * A recoverable error, inspired from https://abseil.io/docs/cpp/guides/status
+ * and https://github.com/abseil/abseil-cpp/blob/master/absl/status/status.h
+ *
+ * This class should be used as immutable.
+ *
+ * For making the success path cheap, use status::success.
+ **/
+class status {
+public:
+  int code;
+  std::string msg;
 
-        status(int code, std::string msg) : code(code), msg(msg) {}
+  status(int code, std::string msg) : code(code), msg(msg) {}
 
-        status(int code) : code(code) {}
+  status(int code) : code(code) {}
 
-        status(std::string msg) : code(-1), msg(msg) {} ///< Generic failure (without code)
+  status(std::string msg) : code(-1), msg(msg) {} ///< Generic failure (without code)
 
-        status() : code(0) {} ///< Generic Success
+  status() : code(0) {} ///< Generic Success
 
-        status(const char *msg) : status(std::string(msg)) {}
+  status(const char *msg) : status(std::string(String(F(msg)).c_str())) {}
 
-        /// Usage like status("Foo %d bar %s baz", 3, "bling");
-        template<typename... Args>
-        status(const char* format, Args... args) : status(small_sprintf(format, args...)) {}
+  /// Usage like status("Foo %d bar %s baz", 3, "bling");
+  template <typename... Args>
+  status(const char *format, Args... args) : status(small_sprintf(String(F(format)).c_str(), args...)) {}
 
-        bool is_ok() const { return code == 0; }
-        operator bool() const { return is_ok(); }
+  bool is_ok() const { return code == 0; }
 
-        /// Syntactic sugar
-        static status success() { return status(0); }
-    };
-} // end of namespace
+  operator bool() const { return is_ok(); }
 
-
+  /// Syntactic sugar
+  static status success() { return status(0); }
+};
+} // namespace utils
