35f94a23d5005f394aaaaa3266268bd0b1219942
SvenK <git+sveto@svenk.org>
2024-09-10 18:49:45 +0200

    SHBlock calibration: Exposing functions to client, but only for testing.
    This reimplements/cherry-picks parts of commit 72b3f460e554a36cf32cfa821cbe22c654e40134 on branch 191b-*

--- lib/platform-lucidac/src/block/shblock.cpp
+++ lib/platform-lucidac/src/block/shblock.cpp
@@ -40,7 +40,18 @@ FLASHMEM void blocks::SHBlock::compensate_hardware_offsets(uint32_t track_time, 
 }
 
 FLASHMEM utils::status blocks::SHBlock::config_self_from_json(JsonObjectConst cfg) {
-  return utils::status("SHBlock does not accept configuration");
+  //return utils::status("SHBlock does not accept configuration");
+
+  // FOR TESTING
+  if(cfg.containsKey("state")) {
+    if(cfg["state"] == "TRACK") set_state(blocks::SHBlock::State::TRACK);
+    else if(cfg["state"] == "INJECT") set_state(blocks::SHBlock::State::INJECT);
+    else if(cfg["state"] == "GAIN_ZERO_TO_SEVEN") set_state(blocks::SHBlock::State::GAIN_ZERO_TO_SEVEN);
+    else if(cfg["state"] == "GAIN_EIGHT_TO_FIFTEEN") set_state(blocks::SHBlock::State::GAIN_EIGHT_TO_FIFTEEN);
+    else return utils::status("Unknown target state for SH Block");
+  }
+  return utils::status(0);
+
 }
 
 FLASHMEM
